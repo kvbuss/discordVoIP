965c2b498bc76322da3037901418ada5efac2ef0
kvbuss <61243251+kvbuss@users.noreply.github.com>
2020-09-22 16:20:34 +0200

    Add files via upload

---
+++ discordVoIP/__resource.lua
@@ -0,0 +1,12 @@
+resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
+
+description 'DiscordVoIP by zelek#6666'
+name 'DiscordVoIP'
+author 'zelek#6666'
+contact 'zelek#6666'
+
+client_script "client.lua"
+server_script "server.lua"
+files {
+    'playerDB.json'
+}
